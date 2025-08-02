df00f028d32c9c1886ff91dd0c03aa0b7bcee0b7
EKRAM-7 <ikram93950@gmail.com>
2025-08-02 13:00:59 +0530

    added real time update feature when a player joins in

--- Components/ClientRoom.js
+++ Components/ClientRoom.js
@@ -1,30 +1,57 @@
 "use client"
 
-import { auth, db } from "@/lib/firebaseConfigs";
+import { auth, db, rtdb } from "@/lib/firebaseConfigs";
 import { onAuthStateChanged } from "firebase/auth";
-import { doc, getDocs, collection } from "firebase/firestore";
-import { useEffect } from "react";
+import { ref, get, set, onValue } from "firebase/database";
+import { doc, getDocs, collection, getDoc } from "firebase/firestore";
+import { useEffect, useState } from "react";
 
 
 export default function ClientRoom({ roomCode }) {
- 
 
+    let [playerIds, setPlayerIds] = useState([]);
+    let [playerNames, setPlayerNames] = useState([]); 
     useEffect(() => {
-        onAuthStateChanged(auth, async (currentUser) => {
-            if (currentUser) {
-                console.log("User is signed in with", currentUser.uid)
-                const querySnapshot = await getDocs(collection(db, "users"));
-                querySnapshot.forEach((doc) => {
-                    console.log(doc.data(), doc.id);
-                })
-            } else {
-                console.log("User is not signed in, Please consider signing in")
-            }
+        const playersRef = ref(rtdb, `room/${roomCode}/players`);
+        onValue(playersRef, async (snapshot) => {
+            fetchPlayers();
         })
+
+       async function fetchPlayers() {
+            let playerNames = [];
+            let playersRef = ref(rtdb, `room/${roomCode}/players`);
+            let snapshot = await get(playersRef);
+            let idArray = snapshot.val(); //O
+            let playerNamesArray = [];
+            for(let id of idArray) {
+                let playerNameRef = doc(db, "users", id);
+                let docSnap = await getDoc(playerNameRef);
+                docSnap = docSnap.data().username;
+                playerNamesArray.push(docSnap);
+            }
+            setPlayerIds(idArray);
+            setPlayerNames(playerNamesArray);
+
+
+        }
+
+        fetchPlayers();
+
     }, [])
+
 
     return (
         <div>
+            <h1>Players</h1>
+            <ul>
+                {
+                    playerNames.map((playerName, i) => (
+                        <li key={i}>
+                            {playerName}
+                        </li>
+                    ))
+                }
+            </ul>
             <h1>Room Code : {roomCode}</h1>
         </div>
     )
